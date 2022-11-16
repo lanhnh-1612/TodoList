f1acfd062bf47b8acf4b9ccdb49f589aaea07ea4
Lanh Nguyen <honglanh1994@gmail.com>
2022-11-16 15:54:42 +0700

    crud work

---
+++ views/work/edit.php
@@ -0,0 +1,69 @@
+<?php require APP_ROOT . '/views/shared/header.php' ?>
+<!--Start content -->
+<div class="app-main__inner">
+    <div class="app-page-title">
+        <div class="page-title-wrapper">
+            <div class="page-title-heading">
+                <div class="page-title-icon">
+                    <i class="fa fa-plus icon-gradient bg-happy-itmeo">
+                    </i>
+                </div>
+                <div>
+                    <h5>Edit work</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main-card card main-card-custom">
+        <div class="card-body">
+            <form enctype="multipart/form-data" method="POST" novalidate="" action="<?php echo BASE_URL; ?>/work/update" accept-charset="UTF-8" id="" name="create_exhibition_form" class="">
+                <div class="form-row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="name">Work name (*)</label>
+                            <input type="hidden" name="id" value="<?php echo $data['oldWork']['id'] ?>">
+                            <input type="text" class="form-control" id="name" name="name" maxlength="255" value="<?php echo (isset($data['dataOld']['name']) ? $data['dataOld']['name'] : $data['oldWork']['name']) ?>" placeholder="Work name" required>
+                            <?php echo (isset($data['errors']) && array_key_exists('name', $data['errors']) ? '<span style="color:red">' . $data['errors']['name'] . '</span>' : false) ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">Starting date (*)</label>
+                            <input type="date" class="form-control" id="" name="starting_date" maxlength="255" value="<?php echo (isset($data['dataOld']['starting_date']) ? $data['dataOld']['starting_date'] : $data['oldWork']['starting_date']) ?>" required>
+                            <?php echo (isset($data['errors']) && array_key_exists('starting_date', $data['errors']) ? '<span style="color:red">' . $data['errors']['starting_date'] . '</span>' : false) ?>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">Ending date (*)</label>
+                            <input type="date" class="form-control" id="" name="ending_date" maxlength="255" value="<?php echo (isset($data['dataOld']['ending_date']) ? $data['dataOld']['ending_date'] : $data['oldWork']['ending_date']) ?>" required>
+                            <?php echo (isset($data['errors']) && array_key_exists('ending_date', $data['errors']) ? '<span style="color:red">' . $data['errors']['ending_date'] . '</span>' : false) ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="name">Status (*)</label>
+                            <select class="custom-select" id="status" name="status">
+                                <option value="">--All status--</option>
+                                <?php foreach ($data['statuses'] as $value) { ?>
+                                    <option <?php if ((isset($data['dataOld']['status']) && $data['dataOld']['status'] == $value) || (isset($data['oldWork']['status']) && $data['oldWork']['status'] == $value)) echo 'selected'; ?> value="<?php echo $value ?>"><?php echo $value ?></option>
+                                <?php } ?>
+                            </select>
+                            <?php echo (isset($data['errors']) && array_key_exists('status', $data['errors']) ? '<span style="color:red">' . $data['errors']['status'] . '</span>' : false) ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button onclick="history.back()" class="mb-2 mr-2 btn-icon btn-pill btn btn-outline-secondary"><i class="fa fa-undo btn-icon-wrapper"></i> Return</button>
+                    <button class="mb-2 mr-2 btn-icon btn-pill btn btn-outline-success"><i class="fa fa-plus btn-icon-wrapper"> </i> Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!--End content -->
+<?php require APP_ROOT . '/views/shared/footer.php' ?>
